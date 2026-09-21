5f16aec7bbdd89f331029b8c66708edf380b5f92
sayalimulik11 <sayalimulik11@gmail.com>
2026-09-21 13:51:05 +0530

    Task 2: Configure admin.py with Question, Choice inlines

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# Import all models
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
 
 
 class LessonInline(admin.StackedInline):
@@ -22,9 +31,15 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'course', 'question_grade']
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
